951f1db62a5201eea20ceeeb510766cc2712b261
Dhafa Ubaid <99788310+DhafaUbaid@users.noreply.github.com>
2022-03-09 13:40:53 +0700

    Add files via upload

---
+++ 058_no 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int kode[] = {001, 002, 003, 004, 005};
+	const char* nama_barang[] = {"Penghapus", "Buku", "Pensil", "Rautan", "Penggaris"};
+	int jml[] = {4, 3, 2, 3, 5};
+	int harga[] = {1000, 1500, 2000, 1000, 500};
+	
+	int total_item = 0;
+	int total_pembelian = 0;
+	int length_item = (sizeof(jml) / sizeof(*jml));
+	int length_pembelian = (sizeof(harga) / sizeof(*harga));
+	
+	for(int i = 0; i < length_item; i++) {
+ 
+        total_item += jml[i];
+        total_pembelian += jml[i] * harga[i];
+    }
+
+	
+	cout << "Jumlah Item = " << total_item << endl;
+	cout << "Total Pembelian = " << total_pembelian << endl;
+	
+	return 0;
+}
+
